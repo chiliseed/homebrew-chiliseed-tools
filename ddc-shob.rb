0dac8b36361428d7a4444ba1c686815c6202903a
Altonotch <info@altonotch.com>
2020-06-28 15:36:38 +0300

    Fix formula name and add version
    Signed-off-by: Altonotch <info@altonotch.com>

--- ddc-shob.rb
+++ ddc-shob.rb
@@ -1,7 +1,8 @@
-class Delta < Formula
+class DdcShob < Formula
     desc "Command line utility to simplify developers life. Short and precise cli commands to interact with django (or other python) server running in docker container via docker compose."
     homepage "https://github.com/chiliseed/django-compose-shob"
     url "https://github.com/chiliseed/django-compose-shob/releases/download/0.3.0/ddc-shob-0.3.0.darwin_amd64.tar.gz"
+    version "v0.3.0"
     sha256 "803c9ac56d8ba5018e97ffa81c08e74bf4518997673ec0b92f896a8cb56ed6da"
 
     bottle :unneeded
