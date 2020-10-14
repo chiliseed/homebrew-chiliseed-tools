5f66207d1d4ce9f38c88ffa6db6a496c0c278b86
Altonotch <info@altonotch.com>
2020-10-14 16:31:40 +0300

    Fix chiliseed cli url

--- chiliseed.rb
+++ chiliseed.rb
@@ -1,7 +1,7 @@
 class Chiliseed < Formula
   desc "Chiliseed command line utility to interact with the hub - aws infrastructure management service."
     homepage "https://github.com/chiliseed/cli"
-    url "https://github.com/chiliseed/cli/releases/download/0.1.2/chiliseed-0.1.2.darwin_amd64.tar.gz"
+    url "https://github.com/chiliseed/cli/releases/download/0.1.2/chiliseed_0.1.2.darwin_amd64.tar.gz"
     version "v0.1.2"
     sha256 "7e616c11d8660fffa1edeaf4209b9a8ecb0789427154c29b45e4a3b787d6203a"
 
